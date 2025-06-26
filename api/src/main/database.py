4025731ea074ef4f9af21cf778e5019c97b38bf1
Eric Warren <eswarren18@gmail.com>
2025-06-26 12:54:17 -0600

    Added test router

--- api/src/main/database.py
+++ api/src/main/database.py
@@ -1,5 +1,3 @@
-# api/src/main/database.py
-
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
@@ -7,13 +5,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DB_USER = os.getenv("POSTGRES_USER")
-DB_PASSWORD = os.getenv("POSTGRES_PASSWORD")
-DB_HOST = os.getenv("POSTGRES_HOST", "localhost")
-DB_PORT = os.getenv("POSTGRES_PORT", "5432")
-DB_NAME = os.getenv("POSTGRES_DB")
-
-DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
--- api/src/main/test_db.py
+++
@@ -1,8 +0,0 @@
-from .database import drop_tables
-
-def test_db():
-    drop_tables()
-    print("Tables dropped successfully!")
-
-if __name__ == "__main__":
-    test_db()
